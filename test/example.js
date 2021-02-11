f2a3d01907cadc48926a474ce61da3832c1e2c19
NuclearRedeye <github@mneah.com>
2021-02-11 17:23:44 +0000

    chore: update test to check url and title

--- test/example.js
+++ test/example.js
@@ -2,6 +2,8 @@ describe('First Steps with with WDIO', () => {
   it('Load http://www.example.com', () => {
     browser.url('http://www.example.com');
     browser.pause(3000);
+    expect(browser).toHaveUrl('http://www.example.com/');
+    expect(browser).toHaveTitle('Example Domain');
   })
 
   it('Load https://webdriver.io', () => {

b7f9cf74ce0d58cd57d88491da65fbf8bbd75999
NuclearRedeye <github@mneah.com>
2021-02-12 08:37:25 +0000

    chore: add new test to set value of input elements

---
+++ test/actions.js
@@ -0,0 +1,15 @@
+describe('Browser Actions', () => {
+  it('can set the value of an input element', () => {
+    browser.url('https://devexpress.github.io/testcafe/example/');
+    const input = $('#developer-name');
+    input.waitForExist();
+    input.setValue('Joe Bloggs');
+    browser.pause(3000);
+    input.clearValue();
+    browser.pause(3000);
+    input.addValue('Joe');
+    input.addValue(' ');
+    input.addValue('Bloggs');
+    browser.pause(3000);
+  })
+})
--- test/example.js
+++ test/example.js
@@ -1,4 +1,4 @@
-describe('First Steps with with WDIO', () => {
+describe.skip('First Steps with with WDIO', () => {
   it('Load http://www.example.com', () => {
     browser.url('http://www.example.com');
     browser.pause(3000);

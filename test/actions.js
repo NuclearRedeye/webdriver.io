f11528d7f8a8fcc7e22b94110e2268d30253eaea
NuclearRedeye <github@mneah.com>
2021-02-12 09:11:52 +0000

    chore: add tests for radio buttons and checkboxes

--- test/actions.js
+++ test/actions.js
@@ -5,13 +5,15 @@ describe('Browser Actions', () => {
     const input = $('#developer-name');
     input.waitForExist();
     input.setValue('Joe Bloggs');
-    browser.pause(3000);
+    browser.pause(1000);
     input.clearValue();
-    browser.pause(3000);
+    browser.pause(1000);
     input.addValue('Joe');
+    browser.pause(500);
     input.addValue(' ');
+    browser.pause(500);
     input.addValue('Bloggs');
-    browser.pause(3000);
+    browser.pause(1000);
   })
 
   it('can click a button', () => {
@@ -19,5 +21,19 @@ describe('Browser Actions', () => {
     button.waitForExist();
     button.click();
     button.doubleClick();
+  })
+
+  it('can select a radio button', () => {
+    const button = $('#linux');
+    button.waitForExist();
+    button.click();
+    browser.pause(1000);
+  })
+
+  it('can select a checkbox', () => {
+    const checkbox = $('#remote-testing');
+    checkbox.waitForExist();
+    checkbox.click();
+    browser.pause(1000);
   })
 })
